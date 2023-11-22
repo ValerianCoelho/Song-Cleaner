7cd40b255c2f0b81b13f334e04d7dd4c4a48554d
Valerian Coelho <valerian27012004@gmail.com>
2023-11-22 19:24:25 +0530

    Entry Component Added

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,18 @@
+<script>
+  import Button from './lib/Button.svelte';
+  import Entry from './lib/Entry.svelte';
+</script>
+
 <main class="container">
-  Hello his
+  <Button value="Browse"/>
+  <Entry/>
 </main>
+
+<style>
+  :global(*) {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/lib/Entry.svelte
@@ -0,0 +1,15 @@
+<input type="text">
+
+<style>
+    input[type="text"] {
+        background-color: #35363A;
+        border: 2px solid #565C5E;
+        color: white;
+        padding: 5px;
+        border-radius: 5px;
+        outline: none;
+    }
+    input[type="text"]:focus {
+        border: 2px solid #879194;
+    }
+</style>
