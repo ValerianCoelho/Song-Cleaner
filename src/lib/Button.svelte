ae3aad021a0ea15734632e4461e2cbd903f33f67
Valerian Coelho <valerian27012004@gmail.com>
2023-11-22 19:24:12 +0530

    Button Component Added

---
+++ src/lib/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let value;
+</script>
+
+<button>
+    {value}
+</button>
+
+<style>
+    button {
+        color: white;
+        background-color: #1F6AA4;
+        border: none;
+        padding: 5px 20px;
+        font-weight: 100;
+        border-radius: 5px;
+        transition: .1s all;
+    }
+    button:hover {
+        background-color: #18466F;
+    }
+</style>
