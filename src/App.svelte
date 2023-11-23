73d5d1ae748a7fa5eadb0f635a52d8831bf359f0
Valerian Coelho <valerian27012004@gmail.com>
2023-11-23 13:40:25 +0530

    Page 1 Half Done

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,11 @@
 <script>
-  import Button from './lib/Button.svelte';
-  import Entry from './lib/Entry.svelte';
-  import Text from './lib/Text.svelte';
-  import Textbox from './lib/Textbox.svelte';
+  import PageOne from "./pages/PageOne.svelte";
+
+
 </script>
 
 <main class="container">
-  <Button value="Browse"/>
-  <Entry/>
-  <Textbox/>
-  <Text text="Hello World"/>
+  <PageOne/>
 </main>
 
 <style>
---
+++ src/pages/PageOne.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Button from '../lib/Button.svelte';
+    import Entry from '../lib/Entry.svelte';
+    import Text from '../lib/Text.svelte';
+    import Textbox from '../lib/Textbox.svelte';
+</script>
+
+<div class="page-1">
+    <div class="title">Rename Songs</div>
+    <div class="inner-frame">
+        <div class="horizontal-view-box">
+            <Entry/>
+            <Button value="Browse"/>
+        </div>
+        <div class="horizontal-view-box">
+            <Entry/>
+            <Button value="Browse"/>
+        </div>
+        <Button value="Next"/>
+    </div>    
+</div>
+
+<style>
+    .page-1 {
+        padding: 10px;
+        background-color: #1E1E1E;
+    }
+    .title {
+        color: white;
+        font-size: 20px;
+    }
+    .inner-frame {
+        background-color: #2B2B2B;
+    }
+</style>
