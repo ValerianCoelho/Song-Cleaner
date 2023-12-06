e8a938e529e654c98c323cbcf1e0c9409ba7fd4c
Valerian Coelho <valerian27012004@gmail.com>
2023-12-06 20:19:18 +0530

    Tried to Access Files from Folder

--- src/pages/PageOne.svelte
+++ src/pages/PageOne.svelte
@@ -1,16 +1,30 @@
 <script>
-  import { open } from "@tauri-apps/api/dialog"
-  async function DialogOpen() {
-    try {
-      const confirmDialog = await open({
-        directory: true
-      });
-      console.log(confirmDialog)
-    }
-    catch(error) {
-      console.log(error)
+  import { open } from "@tauri-apps/api/dialog";
+  import { readDir, BaseDirectory } from '@tauri-apps/api/fs';
+
+  function processEntries(entries) {
+    for (const entry of entries) {
+      console.log(`Entry: ${entry.path}`);
+      if (entry.children) {
+        processEntries(entry.children)
+      }
     }
   }
+
+  async function DialogOpen() {
+    try {
+      const folderLocation = await open({
+        directory: true
+      });
+      console.log(folderLocation);
+
+      const entries = await readDir('', { dir: BaseDirectory.Desktop, recursive: true });
+      processEntries(entries);
+    } 
+    catch(error) {
+      console.log(error);
+    }
+}
 </script>
 
 <div class="page-1">
