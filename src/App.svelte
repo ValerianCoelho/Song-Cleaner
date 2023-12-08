ad30c0f6f606143feab57ee9c10d38f6c6886db5
Valerian Coelho <valerian27012004@gmail.com>
2023-12-08 10:27:16 +0530

    Header Component Made

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,24 @@
 <script>
-  import PageOne from "./pages/PageOne.svelte";
-
+  import SongCleaner from "./pages/SongCleaner.svelte";
 
 </script>
 
 <main class="container">
-  <PageOne/>
+  <SongCleaner/>
 </main>
 
 <style>
+  :root {
+    --dark-1: #454B56;
+    --dark-2: #2A2E36;
+    --dark-3: #202228;
+    --blue-1: #1F6AA4;
+  }
   :global(*) {
     margin: 0;
     padding: 0;
     font-family: sans-serif;
     box-sizing: border-box;
+    background-color: var(--dark-2);
   }
 </style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,16 @@
+<header>
+  Song Cleaner
+</header>
+
+<style>
+  header {
+    border-bottom: 1px solid var(--dark-1);
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.6);
+    height: 40px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    padding-left: 10px;
+    color: white;
+  }
+</style>
