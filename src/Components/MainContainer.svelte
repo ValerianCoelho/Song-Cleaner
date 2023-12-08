6a9274208970280e70857b78977828e34a47fefe
Valerian Coelho <valerian27012004@gmail.com>
2023-12-08 12:57:20 +0530

    Next Button Added

--- src/Components/MainContainer.svelte
+++ src/Components/MainContainer.svelte
@@ -1,43 +1,5 @@
 <script>
-  import Button from "../lib/Button.svelte";
-  import Entry from "../lib/Entry.svelte";
-
+  import StartPage from "../pages/StartPage.svelte";
 </script>
-<div class="main-container">
-	<div class="browse-source">
-    <div class="entry">
-      <Entry/>
-    </div>
-    <div class="button">
-      <Button value="Browse"/>
-    </div>
-  </div>
-	<div class="browse-destination">
-    <div class="entry">
-      <Entry/>
-    </div>
-    <div class="button">
-      <Button value="Browse"/>
-    </div>
-  </div>
-</div>
 
-<style>
-	.main-container {
-		background-color: var(--dark-2);
-    height: 100vh;
-	}
-  .browse-source, .browse-destination {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 10px 20px;
-    gap: 15px;
-  }
-  .entry {
-    flex-grow: 1;
-  }
-  .button {
-    height: 40px;
-  }
-</style>
+<StartPage/>
---
+++ src/pages/StartPage.svelte
@@ -0,0 +1,51 @@
+<script>
+    import Button from "../lib/Button.svelte";
+    import Entry from "../lib/Entry.svelte";
+  
+  </script>
+  <div class="start-page">
+      <div class="browse-source">
+      <div class="entry">
+        <Entry/>
+      </div>
+      <div class="button">
+        <Button value="Browse"/>
+      </div>
+    </div>
+      <div class="browse-destination">
+      <div class="entry">
+        <Entry/>
+      </div>
+      <div class="button">
+        <Button value="Browse"/>
+      </div>
+    </div>
+    <div class="next-btn">
+      <Button value="Next"/>
+    </div>
+  </div>
+  
+  <style>
+    .start-page {
+        background-color: var(--dark-2);
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+        align-items: center;
+    }
+    .browse-source, .browse-destination {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 10px 20px;
+      gap: 15px;
+      width: 100%;
+    }
+    .entry {
+      flex-grow: 1;
+    }
+    .button, .next-btn {
+      height: 40px;
+    }
+  </style>
